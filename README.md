df6a3b9df4b82aad5f15505705e81304aa2918b4
Aldianfp <aldianfarhanputra@gmail.com>
2025-06-26 17:29:36 +0700

    Updating README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mission2Inter-FE-ReactJS
+
+Pada mission kali ini ditambahkan fitur CRUD pada aplikasi
+Fitur CRUD terdapat pada folder components->molecules->CRUD.jsx
+
+Fitur CRUD ditampilkan pada CARD article file Collection.jsx
--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -3,8 +3,6 @@ import HomeLayouts from "../layouts/HomeLayouts";
 import Hero from "../components/molecules/Hero";
 import Collection from "../components/molecules/Collection";
 import CTA from "../components/molecules/CTA";
-import DataForm from "../components/molecules/DataForm";
-import DataList from "../components/molecules/DataList";
 
 export default function Beranda() {
   return (
